ca6c1f199cd978278af413da06af58293b4e4064
gabrielhicks <gabrielhicks@mail.com>
2020-09-05 15:57:47 -0400

    finished getrequest and parsing json method

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_accessor :url
+
+    # URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+    # method that sends a GET request to the URL passed in on initialization. This method should return the body of the response.
+    uri = URI.parse(self.url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+    end
+
+    def parse_json
+    # method should use get_response_body to send a request, 
+    # then return a Ruby Array or Hash made up of data converted from the response of that request.
+    data = JSON.parse(self.get_response_body)
+    end
+
+end
